59a7630dad7fabf31af153fe6834ee3953912fef
+ <+>
2022-07-07 11:01:50 -0400

    commit tree 925780020fb9acef3aaf1e7573273af5b0ab2ee3

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+require "pry"
+require_relative './user'
 
+class Student < User
+
+  attr_accessor :knowledge
+
+  def initialize 
+    @knowledge = []
+  end
+
+  def learn(information)
+    @knowledge << information
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
